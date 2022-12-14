3fd080f0e1873ab7efab4d453f0a21074aad1a79
Ryan1431 <ryan1431@gmail.com>
2022-12-14 13:04:03 -0500

    Finish dash view layout and make responsive

--- frontend/src/components/ui/PrimaryButton.tsx
+++ frontend/src/components/ui/PrimaryButton.tsx
@@ -1,16 +1,24 @@
 import './PrimaryButton.css';
 
-// interface PrimaryButtonProps {
+import { IoIosFlash } from 'react-icons/io';
 
-// }
+interface PrimaryButtonProps {
+  text: string,
+  onClick?: (...args: any) => void,
+  logo?: boolean,
+  colorTheme?: string,
+}
 
-export const PrimaryButton = () => {
+export const PrimaryButton = ({text, onClick, logo, colorTheme}: PrimaryButtonProps) => {
 
-
+  if (colorTheme) {};
 
   return (
-    <div>
-      
+    <div className='PrimaryButton' onClick={onClick}>
+      <h2>{text}</h2>
+      {logo && <div className='PrimaryButton-logo'>
+        <IoIosFlash size={'67%'} />
+      </div>}
     </div>
   )
 }
