199f033b77fc274266ceebb382141fa8f50f9f74
Ryan1431 <ryan1431@gmail.com>
2022-12-16 13:30:46 -0500

    Fix button icon positioning on mobile

--- frontend/src/components/ui/PrimaryButton.tsx
+++ frontend/src/components/ui/PrimaryButton.tsx
@@ -3,24 +3,28 @@ import './PrimaryButton.css';
 import { IoIosFlash } from 'react-icons/io';
 import clsx from 'clsx';
 import { useAppSelector } from '../../utility/hooks';
+import { IconType } from 'react-icons/lib';
 
 interface PrimaryButtonProps {
   text: string,
   onClick?: (...args: any) => void,
   logo?: boolean,
+  Icon?: IconType,
   altColor?: boolean,
   transparent?: boolean,
 }
 
-export const PrimaryButton = ({text, onClick, logo, altColor, transparent}: PrimaryButtonProps) => {
+export const PrimaryButton = ({text, onClick, Icon = IoIosFlash, logo, altColor, transparent}: PrimaryButtonProps) => {
 
   const altTheme = useAppSelector(s => s.theme.buttonSecondary);
+
+
 
   return (
     <div className={clsx('PrimaryButton', 'noselect')} style={{background: altColor ? altTheme : ''}} onClick={onClick}>
       <h2>{text}</h2>
       {logo && <div className='PrimaryButton-logo'>
-        <IoIosFlash size={'67%'} />
+        <Icon size={'28px'} />
       </div>}
     </div>
   )
