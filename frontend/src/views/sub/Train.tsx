d13e9973ec5d7716789a1d6287cf7d96d2ae23f3
Ryan1431 <ryan1431@gmail.com>
2022-12-21 18:24:20 -0500

    Setup dash view skeleton

--- frontend/src/views/sub/Train.tsx
+++ frontend/src/views/sub/Train.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { PrimaryButton } from '../../components/ui/PrimaryButton';
+import { RoutineType } from '../../types/RoutineType';
+import { days } from '../../utility/data/days_months';
 import './Train.css';
 
 // interface TrainProps {
@@ -6,11 +10,32 @@ import './Train.css';
 
 export const Train = () => {
 
+  const date = new Date();
+
+  const [scheduled] = useState<RoutineType[]>([]);
+  const [completed] = useState<RoutineType[]>([]);
 
 
   return (
-    <div>
-      
+    <div className='Train'>
+
+      {scheduled.length ? <div className='Train-scheduled'>
+        <p>Your {days[date.getDay()]} Workouts</p>
+        <p>{completed.length} / {scheduled.length} complete</p>
+      </div> : <div className='Train-noscheduled'>
+        <p>You have not scheduled any workouts for today.</p>
+      </div>}
+
+
+      <div className='Train-actions'>
+        {!!scheduled.length && <div className='Train-hr-text'>
+          <hr></hr>
+          <p>or</p>
+          <hr></hr>
+        </div>}
+        <PrimaryButton text='Select a workout' style={{height: '50px'}} icon={'logo'} />
+        <PrimaryButton text='Start a custom session' altColor style={{height: '50px'}} icon={'logo'} />
+      </div>
     </div>
   )
 }
