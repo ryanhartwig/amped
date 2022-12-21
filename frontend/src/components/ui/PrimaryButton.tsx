a06623d4558c78a3e3c89a3668b553e82bcbf36d
Ryan1431 <ryan1431@gmail.com>
2022-12-21 16:42:19 -0500

    Add textarea to allow user to add notes for a given exercise. +Minor layout fixes

--- frontend/src/components/ui/PrimaryButton.tsx
+++ frontend/src/components/ui/PrimaryButton.tsx
@@ -10,17 +10,19 @@ interface PrimaryButtonProps extends React.DetailedHTMLProps<React.HTMLAttribute
   icon?: IconType | 'logo',
   altColor?: boolean,
   className?: string,
+  iconSize?: number,
+  fontSize?: number | '',
 }
 
-export const PrimaryButton = ({text, icon, altColor, className, ...props}: PrimaryButtonProps) => {
+export const PrimaryButton = ({text, fontSize = '', icon, iconSize = 19, altColor, className, ...props}: PrimaryButtonProps) => {
   const Icon = icon ? icon === 'logo' ? IoIosFlash : icon : undefined;
   const altTheme = useAppSelector(s => s.theme.buttonSecondary);
 
   return (
     <div {...props} className={clsx('PrimaryButton', 'noselect') + ' ' + className || ''} style={{background: altColor ? altTheme : '', ...props.style}} >
-      <h2>{text}</h2>
+      <h2 style={{fontSize}}>{text}</h2>
       {Icon && <div className='PrimaryButton-logo'>
-        <Icon size={'19px'} />
+        <Icon size={iconSize} />
       </div>}
     </div>
   )
