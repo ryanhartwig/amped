088b821b17028e3f62062f75cfa3f90cc4ccfe48
Ryan1431 <ryan1431@gmail.com>
2022-12-22 08:39:54 -0500

    Fix spillover in Train overview

--- frontend/src/views/sub/Train.tsx
+++ frontend/src/views/sub/Train.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { Routine } from '../../components/Routine';
 import { PrimaryButton } from '../../components/ui/PrimaryButton';
 import { RoutineType } from '../../types/RoutineType';
 import { days } from '../../utility/data/days_months';
+import { useAppSelector } from '../../utility/hooks';
 import './Train.css';
 
 // interface TrainProps {
@@ -9,22 +11,34 @@ import './Train.css';
 // }
 
 export const Train = () => {
-
   const date = new Date();
 
-  const [scheduled] = useState<RoutineType[]>([]);
+  const samples = useAppSelector(s => s.workouts.routines)
+  const sample = Array(Math.floor(Math.random() * 12)).fill(true).map(r => samples[Math.floor(Math.random() * samples.length)])
+
+  const { background_alt: background } = useAppSelector(s => s.theme);
+
+  const [scheduled] = useState<RoutineType[]>(sample);
   const [completed] = useState<RoutineType[]>([]);
 
 
   return (
     <div className='Train'>
-
-      {scheduled.length ? <div className='Train-scheduled'>
+      
+      {scheduled.length ? <div className='Train-scheduled-info'>
         <p>Your {days[date.getDay()]} Workouts</p>
         <p>{completed.length} / {scheduled.length} complete</p>
-      </div> : <div className='Train-noscheduled'>
-        <p>You have not scheduled any workouts for today.</p>
-      </div>}
+      </div> : <p>You have not scheduled any workouts for today.</p>}
+      
+      {!!scheduled.length && 
+      <div className='Train-scheduled'>
+        <div className='Train-scheduled-workouts hidescrollbar' style={{background}}>
+          {scheduled.map((r, i) => 
+            <Routine key={`${r}-${i}`} routine={r} />
+          )}
+        </div>
+      </div> 
+      }
 
 
       <div className='Train-actions'>
