3dc70894099925049f8a38b3933f348090633f53
Ryan1431 <ryan1431@gmail.com>
2022-12-22 17:16:47 -0500

    Create InfoBorder component, create Component properties and setup in Overview

--- frontend/src/views/sub/train/Overview.tsx
+++ frontend/src/views/sub/train/Overview.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { InfoBorder } from '../../../components/ui/InfoBorder';
+import { RoutineType } from '../../../types/RoutineType';
+import { useAppSelector } from '../../../utility/helpers/hooks';
 import './Overview.css';
 
 // interface OverviewProps {
 
 // }
 
-export const Overview = () => {
+export const Overview = () => { 
+  const navigate = useNavigate();
 
+  const selectedRoutine = useAppSelector(s => s.session.selectedRoutine);
 
+  const [routine, setRoutine] = useState<RoutineType>();
+
+  useEffect(() => {
+    if (typeof selectedRoutine === 'string' || selectedRoutine?.type !== 'Routine') {
+      navigate('/home/train');
+      return;
+    };
+
+    setRoutine(selectedRoutine);
+  }, [navigate, selectedRoutine]);
 
   return (
     <div className='Overview'>
+      <h2>Overview</h2>
       
+      <div className='Overview-routine'>
+        <InfoBorder title={routine?.name}>
+          <InfoBorder.HeaderLeft>45 min</InfoBorder.HeaderLeft>
+          <InfoBorder.HeaderRight>5</InfoBorder.HeaderRight>
+          <InfoBorder.FooterLeft>things</InfoBorder.FooterLeft>
+          <InfoBorder.FooterRight>wow!</InfoBorder.FooterRight>
+
+          <p>more things</p>
+        </InfoBorder>
+      </div>
     </div>
   )
 }
