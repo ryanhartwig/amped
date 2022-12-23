ab9982f815bf8aadc8b9703d486bbc56c4384070
Ryan1431 <ryan1431@gmail.com>
2022-12-23 13:47:46 -0500

    Use infoborder component to create prev & next navigation buttons in session

--- frontend/src/components/navigation/SessionFooter.tsx
+++ frontend/src/components/navigation/SessionFooter.tsx
@@ -1,6 +1,9 @@
 import './SessionFooter.css';
 
 import { useAppSelector } from '../../utility/helpers/hooks';
+import { IoIosFlash } from 'react-icons/io';
+import { ReactIconButton } from '../ui/ReactIconButton';
+import { InfoBorder } from '../ui/InfoBorder';
 
 export const SessionFooter = () => {
 
@@ -9,7 +12,24 @@ export const SessionFooter = () => {
   return (
     <div className='SessionFooter' style={{background}}>
       <div className='SessionFooter-items'>
-        <p>footer</p>
+        <div className='SessionFooter-nav-button'>
+          <InfoBorder background={background} style={{borderRadius: '7px', borderColor: 'rgba(255,255,255,0.2'}}>
+            <InfoBorder.HeaderLeft><p className='SessionFooter-nav-text'>Previous</p></InfoBorder.HeaderLeft>
+          </InfoBorder>
+        </div>
+        <ReactIconButton 
+          text="Pause" 
+          buttonSize='55px' fontSize='13px' 
+          active
+          style={{flex: '0 0'}}
+        >
+          <IoIosFlash size={30}/>
+        </ReactIconButton>
+        <div className='SessionFooter-nav-button'>
+          <InfoBorder background={background} style={{borderRadius: '7px', borderColor: 'rgba(255,255,255,0.2'}}>
+            <InfoBorder.HeaderRight><p className='SessionFooter-nav-text'>Next</p></InfoBorder.HeaderRight>
+          </InfoBorder>
+        </div>
       </div>
     </div>
   )
