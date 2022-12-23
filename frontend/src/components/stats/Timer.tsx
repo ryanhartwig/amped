7cd92092f6ad309696dc6adc3cd491e080e8320a
Ryan1431 <ryan1431@gmail.com>
2022-12-23 16:56:38 -0500

    Session exercise view info

--- frontend/src/components/stats/Timer.tsx
+++ frontend/src/components/stats/Timer.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { FormatTime } from '../ui/FormatTime';
 import './Timer.css';
 
-// interface TimerProps {
+interface TimerProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  time: number,
+  setTime: React.Dispatch<React.SetStateAction<number>>,
+  showHour?: boolean,
+  className?: string,
+}
 
-// }
-
-export const Timer = () => {
-
-  const [time, setTime] = useState<number>(0);
-
+export const Timer = ({time, setTime, showHour = false, ...props}: TimerProps) => {
   const intervalRef = useRef<NodeJS.Timer>();
 
   useEffect(() => {
@@ -18,11 +18,11 @@ export const Timer = () => {
     }, 1000);
 
     return () => clearInterval(intervalRef.current);
-  }, []);
+  }, [setTime]);
 
   return (
-    <div>
-      <FormatTime seconds={time} showHour />
+    <div {...props}>
+      <FormatTime seconds={time} showHour={showHour} />
     </div>
   )
 }
