2993d6107e6f38b24ec794edfa6913c02681f6f5
Ryan1431 <ryan1431@gmail.com>
2022-12-24 09:26:55 -0500

    Create and move session data down one level to create exerciseData and routineData objects

--- frontend/src/utility/data/sampleExerciseData.ts
+++ frontend/src/utility/data/sampleExerciseData.ts
@@ -5,25 +5,33 @@ export const sampleExerciseData: ExerciseDataType[] = [
   {
     duration: 452,
     exercise_id: 'chestfly',
+    exercise_position: 1,
     routine_data_id: 'chesttriceps',
     id: 'ct-ex1',
+    sets: [],
   }, {
     duration: 347,
     exercise_id: 'tricepextension',
+    exercise_position: 2,
     routine_data_id: 'chesttriceps',
     id: 'ct-ex2',
+    sets: [],
   }, {
     duration: 601,
     exercise_id: 'floorfly',
+    exercise_position: 3,
     routine_data_id: 'chesttriceps',
     id: 'ct-ex3',
+    sets: [],
   }, 
 
   // Shoulders
   {
     duration: 320,
     exercise_id: 'standingshoulderpress',
+    exercise_position: 1,
     routine_data_id: 'chesttriceps',
     id: 'ct-ex3',
+    sets: [],
   }, 
 ]
