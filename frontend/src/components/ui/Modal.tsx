98fcb053074f586259dd37cf1f5e62339fb81d52
Ryan1431 <ryan1431@gmail.com>
2023-01-03 09:11:32 -0500

    Remove summary component and begin to use Modal for summary view

--- frontend/src/components/ui/Modal.tsx
+++ frontend/src/components/ui/Modal.tsx
@@ -14,9 +14,10 @@ interface ModalProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDi
   open: boolean,
   triggerRef: React.MutableRefObject<any>,
   closeText?: string,
+  zIndex?: number,
 }
 
-export const Modal = ({children, onClose, open, triggerRef, closeText, ...divProps}: ModalProps) => {
+export const Modal = ({zIndex = 15, children, onClose, open, triggerRef, closeText, ...divProps}: ModalProps) => {
   const header = React.Children.map(children, (child: any) => child?.type?.displayName === 'Header' ? child : null)
   const content = React.Children.map(children, (child: any) => child?.type?.displayName !== 'Header' ? child : null)
 
@@ -27,13 +28,14 @@ export const Modal = ({children, onClose, open, triggerRef, closeText, ...divPro
 
   return (
     <>
-      {open && <div  className='Modal noselect'>
-        
+      {open && 
+      <div className='Modal noselect' style={{zIndex}}>
         <div className={'Modal-box '} ref={contentRef} style={{background}}>
-          {!!header?.length && <div className='Modal-header'>
+          {!!header?.length && 
+          <div className='Modal-header'>
             <h2>{header}</h2>
             <hr></hr>
-        </div>}
+          </div>}
           <div {...divProps} className={'Modal-content ' + divProps.className ?? ''}>
             {content}
           </div>
