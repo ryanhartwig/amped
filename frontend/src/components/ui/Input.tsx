f6c54fd3719d21968b89c0cb37f6dafa0d4a74d9
Ryan1431 <ryan1431@gmail.com>
2023-01-06 13:29:01 -0500

    Add inputs which accept a goal name and date, with validation

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -4,15 +4,17 @@ interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<H
   className?: string,
   mini?: boolean,
   onEnter?: (...args: any) => void,
+  icon?: React.ReactNode,
 }
 
-export const Input: React.FC<InputProps> = ({onEnter, className = '', mini = false, ...props}) => {
+export const Input: React.FC<InputProps> = ({onEnter, icon, className = '', mini = false, ...props}) => {
   
   return (
     <div className='Input' style={{width: '100%'}}>
       <input {...props} 
         className={`${className} ${mini ? 'mini' : ''}`}
         onKeyDown={(e) => e.key === 'Enter' && onEnter && onEnter()}/>
+      {icon}
     </div>
   )
 }
--- frontend/src/views/sub/Profile.tsx
+++ frontend/src/views/sub/Profile.tsx
@@ -2,15 +2,28 @@ import './Profile.css';
 
 /* React Icons */
 import { IoPersonOutline } from 'react-icons/io5';
+import { BsCalendar } from 'react-icons/bs';
+
 import { useAppSelector } from '../../utility/helpers/hooks';
 import { WeeklyTarget } from '../../components/stats/WeeklyTarget';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { Modal } from '../../components/ui/Modal';
+import { Input } from '../../components/ui/Input';
+import { PrimaryButton } from '../../components/ui/PrimaryButton';
 
 export const Profile = () => {
-
   const { background_alt: background } = useAppSelector(s => s.theme);
 
   const [weeks] = useState<number>(2);
+  const [addGoal, setAddGoal] = useState<boolean>(false);
+  
+  // Input values
+  const [goal, setGoal] = useState<string>('');
+  const [dateValue, setDateValue] = useState<string>('');
+  
+  const [year, month, day] = useMemo<number[]>(() => dateValue.split('-').map(n => Number(n)), [dateValue]);
+  const selectedDate = useMemo<Date>(() => new Date(year, month - 1, day), [day, month, year]);
+  const valid = useMemo(() => selectedDate.getTime() >= new Date().getTime() && goal.length, [goal.length, selectedDate]);
 
   return (
     <div className='Profile'>
@@ -35,9 +48,39 @@ export const Profile = () => {
       {/* Training Goals / Milestones */}
       <div className='Profile-goals'>
         <p>My training goals</p>
-        <div className='Profile-goals-wrapper' style={{background}}>
-          <p className='Profile-goals-add'>+ Add training goal</p> 
+        <div className='Profile-goals-wrapper hidescrollbar' style={{background}}>
+          <p className='Profile-goals-add' onClick={() => setAddGoal(true)}>+ Add training goal</p> 
         </div>
+        
+        {/* Add goal modal */}
+        <Modal open={addGoal} onClose={() => setAddGoal(false)} closeText='Cancel'>
+          <Modal.Header>Add a training goal</Modal.Header>
+          <div className='Profile-goals-content'>
+            <div className='Profile-goals-inputs'>
+              <label htmlFor='PRF-goal'><p className='PRF-label'>Goal label</p></label>
+              <Input id='PRF-goal' 
+                placeholder='eg. 200lb 1RM (bench press)' 
+                value={goal}
+                onChange={(e) => setGoal(e.target.value)}
+              />
+
+              <label htmlFor='PRF-date'><p className='PRF-label'>Goal deadline</p></label>
+              <Input type={'date'} 
+                id='PRF-date'
+                onChange={(e) => setDateValue(e.target.value)}
+                value={dateValue}
+                icon={
+                  <div className='Profile-goals-icon'>
+                    <BsCalendar className='Profile-goals-calendar' size={18} />
+                  </div>
+                } 
+              />
+
+            </div>
+            
+            <PrimaryButton text='Save Goal' icon={'logo'} disabled={!valid} />
+          </div>
+        </Modal>
       </div>
     </div>
   )
