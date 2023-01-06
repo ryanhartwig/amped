8faa2b98d1b56cd42369ad9177707431f53faa55
Ryan1431 <ryan1431@gmail.com>
2023-01-06 13:43:05 -0500

    Create Goal type and implement action to add or patch existing goals in useSlice

--- frontend/src/store/slices/userSlice.ts
+++ frontend/src/store/slices/userSlice.ts
@@ -1,18 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { scheduled, ScheduledState } from "../../types/scheduledState";
+
+interface Goal {
+  deadline: number,
+  goal: string,
+  completed: boolean,
+  id: string,
+}
 
 interface UserState {
   scheduled: ScheduledState,
+  goals: Goal[],
 }
 
 const initialState: UserState = {
   scheduled,
+  goals: [],
 }
 
 export const userReducer = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    addEditGoal: (state, action: PayloadAction<Goal>) => {
+      const index = state.goals.findIndex(g => g.id === action.payload.id);
+
+      if (index === -1) state.goals.push(action.payload);
+      else state.goals[index] = action.payload;
+    },
   }
 });
 
