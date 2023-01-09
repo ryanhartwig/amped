d7b9beb7bb2e9f2d8890a21772ae4811d4df4cc6
Ryan1431 <ryan1431@gmail.com>
2023-01-09 14:43:45 -0500

    install morgan and fix module import error

--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,18 @@
-import express  from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use('/', morgan('dev'));
+
 app.get('/', (_, res) => {
-  res.send('<h1>Express  += sd typescript server</h1>');
+  res.send('<h1>Express += sd typescript server</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log('⚡️', `Server is running at http://localhost:${port}`);
 });
