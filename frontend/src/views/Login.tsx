042a93c4fbdff94da82c3acf81beb46b79f1f99c
Ryan1431 <ryan1431@gmail.com>
2023-01-16 09:15:33 -0500

    Add passport-facebook dep to backend

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import './Login.css';
 
 // interface LoginProps {
@@ -7,10 +8,31 @@ import './Login.css';
 export const Login = () => {
 
 
+  const onAuth = useCallback(() => {
+    
+  }, []);
 
+  
   return (
-    <div>
-      test
+    <div className='Login'>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button onClick={onAuth}>Login with facebook</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button>Login with google</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button>Login with twitter</button>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   )
 }
