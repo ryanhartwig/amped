2e589f6c8046ed64211c5bb018f40210cdf32fe1
Ryan1431 <ryan1431@gmail.com>
2023-01-16 09:04:26 -0500

    Redirect unauthenticated users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import { AddExercise } from './views/sub/AddExercise';
 import { AddRoutine } from './views/sub/AddRoutine';
 import { Overview } from './views/sub/train/Overview';
 import { Session } from './views/sub/train/Session';
+import { GetAuth } from './views/GetAuth';
 
 function App() {
 
@@ -22,18 +23,20 @@ function App() {
     <div className='App' style={{background}}>
       <BrowserRouter>
         <Routes>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="home" element={<Home />}>
-            <Route path="dash" element={<Dash />} />
-            <Route path="routines" element={<Routines />} />
-            <Route path="routines/add-rt" element={<AddRoutine />} />
-            <Route path="routines/add-ex" element={<AddExercise />} />
-            <Route path="train" element={<Train />} />
-            <Route path="train/overview" element={<Overview />} />
-            <Route path="finished" element={<Completed />} />
-            <Route path="profile" element={<Profile />} />
+          <Route path='/' element={<GetAuth />}>
+            <Route path="login" element={<Login />} />
+            <Route path="home" element={<Home />}>
+              <Route path="dash" element={<Dash />} />
+              <Route path="routines" element={<Routines />} />
+              <Route path="routines/add-rt" element={<AddRoutine />} />
+              <Route path="routines/add-ex" element={<AddExercise />} />
+              <Route path="train" element={<Train />} />
+              <Route path="train/overview" element={<Overview />} />
+              <Route path="finished" element={<Completed />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+            <Route path="session" element={<Session />} />
           </Route>
-          <Route path="session" element={<Session />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
