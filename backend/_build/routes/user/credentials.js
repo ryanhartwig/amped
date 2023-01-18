6f7304422c76fac9d4f45f8e1d97e3a546289cee
Ryan1431 <ryan1431@gmail.com>
2023-01-18 08:10:17 -0500

    Create credentials POST route and input validation for creating new user

--- backend/_build/routes/user/credentials.js
+++ backend/_build/routes/user/credentials.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,10 +37,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_promise_router_1 = __importDefault(require("express-promise-router"));
 const db_1 = __importDefault(require("../../db"));
+const bcrypt = __importStar(require("bcrypt"));
 const credentials = (0, express_promise_router_1.default)();
 credentials.get('/unique/:username', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { username } = req.params;
-    const response = yield db_1.default.query('select * from hashes where username = $1', [username]);
+    const response = yield db_1.default.query('select * from local_credentials where username = $1', [username]);
     return res.status(200).json(!!response.rowCount);
+}));
+credentials.post('/new', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { password, user_id, username } = req.body;
+    const hash = yield bcrypt.hash(password, 12);
+    const response = yield db_1.default.query(`
+    insert into local_credentials values (
+      $1, $2, $3
+    ) returning user_id`, [hash, user_id, username]);
+    if (!response.rowCount)
+        return res.status(500).json('Could not create credentials');
+    return res.status(200).json(response.rows[0]);
 }));
 exports.default = credentials;
