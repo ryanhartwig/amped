588acaced442e7d9139a8b9c3e9768d7224e81cc
Ryan1431 <ryan1431@gmail.com>
2023-01-19 12:17:42 -0500

    Create and implement HrText ui component in Login view

---
+++ frontend/src/components/ui/HrText.tsx
@@ -0,0 +1,20 @@
+import './HrText.css';
+
+interface HrTextProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  text: string,
+  background: string,
+  textFontSize?: number | string,
+  textBottomOffset?: number,
+}
+
+export const HrText: React.FC<HrTextProps> = ({text, textBottomOffset, background, textFontSize, ...props}) => {
+
+  return (
+    <div {...props} className={"HrText " + props.className ?? ''} >
+      <hr></hr>
+      <div className='HrText-front' style={{bottom: textBottomOffset}} >
+        <p style={{fontSize: textFontSize, background}}>{text}</p>
+      </div>
+    </div>
+  )
+}
