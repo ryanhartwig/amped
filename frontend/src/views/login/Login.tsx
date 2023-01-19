fa6dda3e2e5a8535492858396948e7888e2047f9
Ryan1431 <ryan1431@gmail.com>
2023-01-19 13:44:41 -0500

    Add amped logo

--- frontend/src/views/login/Login.tsx
+++ frontend/src/views/login/Login.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Logo } from '../../components/ui/Logo';
 import './Login.css';
 
 export const Login = () => {
 
   return (
     <div className='Login'>
+      <Logo style={{scale: '1.5', marginBottom: 25}} hideLogo />
       <div className='Login-form'>
         <Outlet />
       </div>
