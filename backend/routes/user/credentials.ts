d1fae64dd05acef267acfa9cacf5db1897fc7f9d
Ryan1431 <ryan1431@gmail.com>
2023-01-20 09:48:17 -0500

    requre unique email and setup nodemailer to send reset link in api

--- backend/routes/user/credentials.ts
+++ backend/routes/user/credentials.ts
@@ -1,15 +1,48 @@
 import PromiseRouter from 'express-promise-router';
 import db from '../../db';
 import * as bcrypt from 'bcrypt';
+import nodemailer from 'nodemailer';
 
 const credentials = PromiseRouter();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_ADDR,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
 credentials.get('/exists/:username', async (req, res) => {
   const { username } = req.params;
   const response = await db.query('select * from local_credentials where username = $1', [username]);
 
-  return res.status(200).json(response.rows[0]?.username || null)
+  return res.status(200).json(response.rows[0]?.username || null) 
 });
+
+credentials.get('/email/exists/:email', async (req, res) => {
+  const { email } = req.params;
+  const response = await db.query('select * from users where email = $1', [email]);
+
+  return res.status(200).json(response.rows[0]?.email || null);
+});
+
+credentials.get('/reset/:email', async (req, res) => {
+  const { email } = req.params;
+  const response = await db.query('select * from users where email = $1', [email]);
+
+  if (!response.rowCount) return res.status(200).send();
+
+  transporter.sendMail({
+    from: process.env.MAIL_ADDR,
+    to: email,
+    subject: 'AMPED | Password Reset Link',
+    text: 'Test',
+  }, (err) => {
+    if (err) return res.status(500).json(err);
+    else return res.status(200).send();
+  })
+})
 
 credentials.post('/new', async (req, res) => {
   const { password, user_id, username } = req.body;
@@ -25,6 +58,8 @@ credentials.post('/new', async (req, res) => {
 
   if (!response.rowCount) return res.status(500).json('Could not create credentials');
   return res.status(200).json(response.rows[0]);
-})
+});
+
+
 
 export default credentials;
