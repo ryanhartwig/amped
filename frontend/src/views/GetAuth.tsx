0e062da4e5fec8bb2c6764184c58911c39de07d7
Ryan1431 <ryan1431@gmail.com>
2023-01-20 08:18:13 -0500

    Redirect / route to /login

--- frontend/src/views/GetAuth.tsx
+++ frontend/src/views/GetAuth.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import './GetAuth.css';
 
 export const GetAuth = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') navigate('/login');
+  }, [location.pathname, navigate]);
 
   return (
     <div className='GetAuth'>
