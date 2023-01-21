d2935ffcbd7b2af8c76214e08b7baa5ca8f6c13d
Ryan1431 <ryan1431@gmail.com>
2023-01-21 14:21:09 -0500

    Add db folder and file back...

---
+++ backend/db/index.ts
@@ -0,0 +1,9 @@
+import { Pool } from 'pg';
+
+export default new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS || '',
+  port: Number(process.env.PG_PORT),
+});
