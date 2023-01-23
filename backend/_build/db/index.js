75f600721d75de2c2d1450b7a2cf520d6b2dfe5a
Ryan1431 <ryan1431@gmail.com>
2023-01-23 09:27:11 -0500

    Change rejectUnauthorized to false

--- backend/_build/db/index.js
+++ backend/_build/db/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 exports.default = new pg_1.Pool({
-    ssl: process.env.NODE_ENV !== 'production' ? false : {
+    ssl: {
         rejectUnauthorized: false,
     },
     user: process.env.USER,
