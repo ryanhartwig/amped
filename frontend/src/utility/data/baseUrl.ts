33417c6635747a42dc88eaf9c03836d8af3fed33
Ryan1431 <ryan1431@gmail.com>
2023-01-24 15:51:53 -0500

    Change baseurl to use network ip instead of localhost (for mobile testing)

--- frontend/src/utility/data/baseUrl.ts
+++ frontend/src/utility/data/baseUrl.ts
@@ -1,3 +1,3 @@
 import { getEnv } from "../helpers/getEnv";
 
-export const baseUrl = getEnv<string>('https://amped.herokuapp.com/api', 'http://localhost:8000/api');
+export const baseUrl = getEnv<string>('https://amped.herokuapp.com/api', 'http://192.168.2.27:8000/api');
