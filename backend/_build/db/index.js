13b00fbaf688a5e6bd69fe17d9c8898ef0ca5693
Ryan1431 <ryan1431@gmail.com>
2023-01-24 13:45:34 -0500

    Set sameSite with getEnv helper. Successfully able to login & interact with app locally & in production

--- backend/_build/db/index.js
+++ backend/_build/db/index.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
+const baseUrl_1 = require("../baseUrl");
 exports.default = new pg_1.Pool({
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: (0, baseUrl_1.getEnv)({ rejectUnauthorized: false }, false),
     user: process.env.USER,
     host: process.env.HOST,
     database: process.env.PG_DB,
